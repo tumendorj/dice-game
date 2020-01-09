a5a2264f08bacbf20e78c96930ccd1e581b8e22d
tumendorj tserendorj <ts.tumendorj@yahoo.com>
2020-01-09 15:48:54 +0800

    toglogchiin onoog hold hiih

--- app.js
+++ app.js
@@ -19,6 +19,30 @@ document.querySelector(".btn-roll").addEventListener("click", function() {
   var diceNumber = Math.floor(Math.random() * 6) + 1;
   diceBtn.style.display = "block";
   diceBtn.src = "dice-" + diceNumber + ".png";
-  if (activePlayer === 0 && diceNumber !== 1) {
+  if (diceNumber !== 1) {
+    //тоглогчийн оноог нэмэгдүүлэх
+    roundScore += diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+    document.querySelector(".btn-hold").addEventListener("click", function() {
+      scores[activePlayer] += roundScore;
+      document.getElementById("score-" + activePlayer).innerHTML =
+        scores[activePlayer];
+      roundScore = 0;
+      document.getElementById(
+        "current-" + activePlayer
+      ).textContent = roundScore;
+      activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+      document.querySelector(".player-0-panel").classList.toggle("active");
+      document.querySelector(".player-1-panel").classList.toggle("active");
+      diceBtn.style.display = "none";
+    });
+  } else {
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    diceBtn.style.display = "none";
+    //current оноог тэглэж тоглогчийн ээлжийг солих
   }
 });
